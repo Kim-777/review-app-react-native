1d57277fe45b59ad25f6dcb6c34a8732d3ac251c
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-06-21 14:24:50 +0900

    setting pages

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>나의 두번째 네이티브 프로젝트 고고고고고!!</Text>
       <StatusBar style="auto" />
     </View>
   );
